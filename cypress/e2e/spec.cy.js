53cc41b498feffe16f9a8c1c6588a841e72a963c
Ernst Jendritzki <ernstjendritzki@web.de>
2023-10-02 23:20:07 +0200

    First basic tests with cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,14 @@
-describe("The Login Page", () => {
+describe("Test the Homepage", () => {
   beforeEach(() => {
     // reset the database prior to every test
-    cy.exec("npm run db:reset");
+    // cy.exec("npm run db:reset");
   });
 
   it("visit homepage", () => {
     // Visit the home page
     cy.visit("/");
-
-    // We should be redirected to /meine-bestellungen
-    cy.url().should("include", "/isotopedemo.loc");
+    // Assert that the breadcrumb navigation indicates "Home"
+    cy.get(".active.last").should("contain", "Home");
   });
 
   // it logs in successfully
@@ -23,7 +22,102 @@ describe("The Login Page", () => {
 
     cy.login(username, password);
 
-    // We should be redirected to /meine-bestellungen
-    cy.url().should("include", "/meine-bestellungen");
-  }); // it logs in successfully
+    // We should be redirected
+    cy.url().should("include", "/my-order-history");
+
+    // Extend test with Cypress Studio
+    it("create new transaction", () => {});
+  });
+
+  // /* ==== Test Created with Cypress Studio ==== */
+  // it("Buy sth. as a guest", function () {
+  //   /* ==== Generated with Cypress Studio ==== */
+  //   cy.visit("/");
+  //   cy.get(".level_1 > :nth-child(2) > .submenu").click();
+  //   cy.get("li.next > .next").click();
+  //   cy.get("#fmd19_product_136 > .formbody > h3 > a").click();
+  //   cy.get("#quantity_requested_136").clear("2");
+  //   cy.get("#quantity_requested_136").type("2");
+  //   cy.get(".add_to_cart").click();
+  //   cy.get(".iso_confirm").click();
+  //   cy.get(".button_checkout").click();
+  //   cy.get("div.ce_text > :nth-child(3) > a").click();
+  //   cy.get("#ctrl_billingaddress_firstname").clear("T");
+  //   cy.get("#ctrl_billingaddress_firstname").type("Test");
+  //   cy.get("#ctrl_billingaddress_lastname").clear();
+  //   cy.get("#ctrl_billingaddress_lastname").type("Tester");
+  //   cy.get("#ctrl_billingaddress_street_1").clear();
+  //   cy.get("#ctrl_billingaddress_street_1").type("Testweg 1");
+  //   cy.get("#ctrl_billingaddress_postal").clear();
+  //   cy.get("#ctrl_billingaddress_postal").type("11111");
+  //   cy.get("#ctrl_billingaddress_city").clear();
+  //   cy.get("#ctrl_billingaddress_city").type("Testort");
+  //   cy.get(
+  //     "#ctrl_billingaddress_country_chzn > .chzn-single > div > b"
+  //   ).click();
+  //   cy.get("#ctrl_billingaddress_country_chzn_o_186").click();
+  //   cy.get("#ctrl_billingaddress_phone").click();
+  //   cy.get("#ctrl_billingaddress_email").clear("t");
+  //   cy.get("#ctrl_billingaddress_email").type("test@test.de");
+  //   cy.get(".submit_container > .submit").click();
+  //   cy.get(".confirm").click();
+  //   /* ==== End Cypress Studio ==== */
+  // });
+
+  // /* ==== Test Created with Cypress Studio ==== */
+  // it("Buy sth. logged in ", function () {
+  //   /* ==== Generated with Cypress Studio ==== */
+  //   cy.visit("/");
+  //   cy.get("#username").clear("j");
+  //   cy.get("#username").type("j.smith");
+  //   cy.get("#password").clear();
+  //   cy.get("#password").type("johnsmith");
+  //   cy.get("#tl_login_2 > .formbody > .widget-submit > .submit").click();
+  //   cy.get(":nth-child(2) > .level_2 > :nth-child(3) > a").click();
+  //   cy.get("li.next > .next").click();
+  //   cy.get("#fmd68_product_202 > .formbody > h3 > a").click();
+  //   cy.get("#ctrl_size_fmd21_product_202").select("L");
+  //   cy.get("#quantity_requested_207").click();
+  //   cy.get(".toggle_favorites").click();
+  //   cy.get(".iso_confirm").click();
+  //   cy.get(".add_to_cart").click();
+  //   cy.get(".update").click();
+  //   cy.get("h2.grid3").click();
+  //   cy.get(".cart > .button").click();
+  //   cy.get(".row_0 > .col_5 > .edit").click();
+  //   cy.get(":nth-child(1) > .remove > a").click();
+  //   cy.get("#fmd21_product_213 > .formbody").click();
+  //   cy.get(".remove > a").click();
+  //   cy.get(".empty").click();
+  //   cy.get(".level_1 > :nth-child(3) > .sibling").click();
+  //   cy.get("li.next > .next").click();
+  //   cy.get("#fmd68_product_202 > .formbody > h3 > a").click();
+  //   cy.get("#ctrl_size_fmd21_product_202").select("L");
+  //   cy.get(".add_to_cart").click();
+  //   cy.get(".iso_confirm").click();
+  //   cy.get(".cart > .button").click();
+  //   cy.get(".edit").click();
+  //   cy.get("#ctrl_size_fmd21_product_202").select("XL");
+  //   cy.get(".add_to_cart").click();
+  //   cy.get(".iso_confirm").click();
+  //   cy.get(".edit").click();
+  //   cy.get(".back > a").click();
+  //   cy.get(".edit").click();
+  //   cy.get(".back > a").click();
+  //   cy.get(".edit").click();
+  //   cy.get(".remove > a").click();
+  //   /* ==== End Cypress Studio ==== */
+  // });
+
+  // /* ==== Test Created with Cypress Studio ==== */
+  // it("Buy a cd as a guest", function () {
+  //   /* ==== Generated with Cypress Studio ==== */
+  //   cy.visit("/");
+  //   cy.get(".level_1 > :nth-child(2) > .submenu").click();
+  //   cy.get("li.active > .level_2 > li.first > .first").click();
+  //   cy.get("#fmd68_product_139 > .formbody > .details > a").click();
+  //   cy.get(".add_to_cart").click();
+  //   cy.get(".iso_confirm").click();
+  //   /* ==== End Cypress Studio ==== */
+  // });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,11 +24,23 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+// import "@testing-library/cypress/add-commands";
+
 // Fill in a login form
 Cypress.Commands.add("login", (username, password) => {
   // Type the username and password into input fields using their name
   cy.get("input[name=username]").type(username);
   cy.get("input[name=password]").type(`${password}{enter}`, { log: false });
   // our auth cookie should be present
-  // cy.getCookie("your-session-cookie").should("exist");
+  cy.getCookie("PHPSESSID").should("exist");
+});
+
+// Select elements by data-test attribute
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+  return cy.get(`[data-test=${selector}]`, ...args);
+});
+
+// Select elements by data-test attribute mit Wildcard
+Cypress.Commands.add("getBySelLike", (selector, ...args) => {
+  return cy.get(`[data-test*=${selector}]`, ...args);
 });
